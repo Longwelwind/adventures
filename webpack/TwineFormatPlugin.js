920e1009cb4ed2a70f4bf3c721e815efb2dba442
Longwelwind <longwelwind@gmail.com>
2017-09-28 16:39:37 +0200

    Tag to 1.1.0

--- webpack/TwineFormatPlugin.js
+++ webpack/TwineFormatPlugin.js
@@ -60,7 +60,7 @@ TwineFormatPlugin.prototype.apply = function(compiler) {
 
 						let options = {
 							name: "Adventures",
-							version: "1.0.2",
+							version: "1.1.0",
 							author: "Longwelwind",
 							description: "A story format to create RPG stories with health, loot, gold and more. See its <a href=\"https://longwelwind.github.io/adventures/\">documentation</a>",
 							proofing: false,
