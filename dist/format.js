0422d7d79aefe40ab5c832dd960a345e1f13597b
Longwelwind <longwelwind@gmail.com>
2018-01-13 12:35:58 +0100

    Tag v1.1.1

--- dist/format.js
+++ dist/format.js
@@ -1,6 +1,6 @@
 window.storyFormat({
   "name": "Adventures",
-  "version": "1.1.0",
+  "version": "1.1.1",
   "author": "Longwelwind",
   "description": "A story format to create RPG stories with health, loot, gold and more. See its <a href=\"https://longwelwind.github.io/adventures/\">documentation</a>",
   "proofing": false,
